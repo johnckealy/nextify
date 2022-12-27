5b4aeb96dcfd54bb36341c43e0990f856ae9cfab
John C. Kealy <johnckealy.dev@gmail.com>
2022-12-27 08:13:53 +0700

    Add ne home

--- components/themes/OutdoorAdventure/Home.tsx
+++ components/themes/OutdoorAdventure/Home.tsx
@@ -1,7 +1,81 @@
+import Img from '@/components/Img'
+import CTA from '@/components/common/CTA'
+import Navbar from '@/components/Navbar'
+
+
+const content = {
+  quoteText: '"Fuerat aestu carentem habentia spectent tonitrua mutastis locavit liberioris"',
+  quoteAuthor: 'Jim Farrey',
+  cards: [
+    {
+      imageSrc: '/images/hero-bg.jpg',
+      title: 'Everest Camp Trek',
+      body: 'Fuerat aestu carentem habentia spectent tonitrua mutastis locavit liberioris inistra possedit.',
+      cta: {
+        label: 'Learn More',
+        href: '/'
+      }
+    },
+    {
+      imageSrc: '/images/hero-bg.jpg',
+      title: 'Walking Holidays',
+      body: 'Fuerat aestu carentem habentia spectent tonitrua mutastis locavit liberioris inistra possedit.',
+      cta: {
+        label: 'Learn More',
+        href: '/'
+      }
+    }
+  ],
+  block1: {
+    title: 'Explore the world',
+    description: 'Diremit mundi mare undae nunc mixtam tanto sibi. Nubes unda concordi. Fert his. Recessit mentes praecipites locum caligine sui egens erat. Silvas caeli regna.',
+    cta: {
+      label: 'Learn More',
+      href: '/'
+    }
+  },
+  block2: {
+    title: 'UPCOMING TOURS & DESTINATIONS',
+    description: 'Fuerat aestu carentem habentia spectent tonitrua mutastis locavit liberioris. Sinistra possedit litora ut nabataeaque. Setucant coepyterunt perveniunt animal! Concordi aurea nabataeaque seductaque constaque cepit sublime flexi nullus.',
+    cta: {
+      label: 'Learn More',
+      href: '/'
+    }
+  },
+  section1: {
+    mainImage: '/images/section-bg-img.jpg'
+  },
+  gallery: {
+    image1: 'https://source.unsplash.com/pmfJcN7RGiw/w=600',
+    image2: 'https://source.unsplash.com/ZlJ6qFInqQE/w=600',
+    image3: 'https://source.unsplash.com/Pf6e3o0GL4M/w=600',
+    image4: 'https://source.unsplash.com/ZX9s4SA6ePg/w=600',
+  }
+
+}
+
+
+
 const Home = () => {
   return (
-    <h1>Home Page</h1>
-   );
+    <>
+      <Navbar className="z-10 bg-opacity-0 md:text-white" />
+
+
+      <Img src={`https://source.unsplash.com/Bkci_8qcdvQ`} alt="image" className="">
+        <div className='absolute inset-0 bg-black opacity-30' />
+        <div className='relative p-32'>
+          <div className='flex flex-col justify-center items-center'>
+            <div>
+              <h1 className='text-7xl font-montserrat font-bold text-white'>Hello am I inter?</h1>
+              <h2 className='text-white'>Some kind of subtitle</h2>
+              <CTA href='/'>Get started</CTA>
+            </div>
+          </div>
+        </div>
+      </Img>
+    </>
+  );
 }
 
 export default Home;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,15 @@
 import HomePage from '@/components/themes/OutdoorAdventure/Home'
-import Img from '@/components/Img'
-import CTA from '@/components/common/CTA'
+import { NextSeo } from 'next-seo'
 
 
 
 export default function Home() {
   return (
     <>
-      <HomePage />
 
-      <Img src={`https://source.unsplash.com/Bkci_8qcdvQ`} alt="image" className="">
-        <div className='absolute inset-0 bg-black opacity-30' />
-        <div className='relative p-32'>
-          <div className='flex flex-col justify-center items-center'>
-            <div>
-              <h1 className='text-7xl font-montserrat font-bold text-white'>Hello am I inter?</h1>
-              <h2 className='text-white'>Some kind of subtitle</h2>
-              <CTA href='/'>Get started</CTA>
-            </div>
-          </div>
-        </div>
-      </Img>
+      <NextSeo title="Change me I'm the home page!" description="" />
+
+      <HomePage />
 
 
 
